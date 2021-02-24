d43cb20bba43cd747d33a109cd028cdf1b8ccd86
rhebeca abreu <rhebecaabreu@gmail.com>
2021-02-24 01:53:08 -0400

    [NEW] Starting countdown

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,12 +1,14 @@
+import styles from '../styles/components/ExperienceBar.module.css';
+
 export function ExperienceBar() {
   return (
-    <header className="experience-bar">
+    <header className={ styles.experienceBar }>
       <span>0 xp</span> 
       <div>
         <div style={{ width: '50%' }} />
         <span
           style={{ left: '50%' }}
-          className="current-experience"
+          className={ styles.currentExperience }
           >
             300 xp
           </span>
