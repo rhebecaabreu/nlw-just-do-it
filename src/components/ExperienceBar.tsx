5b76b725a954b1d967a694de6e1124fdd2e13e48
rhebeca abreu <rhebecaabreu@gmail.com>
2021-02-24 18:01:29 -0400

    [NEW] Adding context

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,19 +1,24 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar() {
+  const { currentExperience, experienceToNextLevel } = useContext(ChallengesContext);
+  const porcentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
   return (
     <header className={ styles.experienceBar }>
       <span>0 xp</span> 
       <div>
-        <div style={{ width: '50%' }} />
+        <div style={{ width: `${ porcentToNextLevel }`}} />
         <span
-          style={{ left: '50%' }}
+          style={{ left: `${ porcentToNextLevel }`}}
           className={ styles.currentExperience }
           >
-            300 xp
+            { currentExperience } xp
           </span>
       </div>
-      <span> 680 xp </span>
+      <span> { experienceToNextLevel } xp </span>
     </header>
   );
 }
--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,8 +16,10 @@ interface ChallengesContextData {
   currentExperience: number;
   challengesCompleted: number;
   activeChallenge: Challenge;
+  experienceToNextLevel: number;
   levelUp: () => void;
   startNewChallenge: () => void;
+  resetChallenge: () => void;
 }
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
@@ -27,6 +29,8 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [currentExperience, setCurrentExperience] = useState(0);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
   const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
   function levelUp() {
     setLevel(level + 1);
@@ -39,6 +43,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     setActiveChallenge(challenge);
   }
 
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
   return (
     <ChallengesContext.Provider
       value={{
@@ -46,8 +54,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         currentExperience,
         challengesCompleted,
         activeChallenge,
+        experienceToNextLevel,
         levelUp, 
-        startNewChallenge 
+        startNewChallenge,
+        resetChallenge
       }}
     >
       { children }
